f18f38121062a901ec3077929281c0a2270d933a
Paladibot <saveli.shulz@gmail.com>
2023-02-12 23:16:29 +0300

    If_Else

---
+++ HomeWork/Task01_If_Else/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите ваше имя: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "saveli")
+{
+     Console.WriteLine("Yeah Bro, " + userName);
+}
+
+else
+{
+    Console.WriteLine( userName + " ,уходи ");
+
+}
+
