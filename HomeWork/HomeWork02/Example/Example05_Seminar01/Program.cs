1f04a07d1b8f400f1bcbac8466a8e5a76c67b07c
Paladibot <saveli.shulz@gmail.com>
2023-02-22 16:35:20 +0300

    22.02.23

---
+++ HomeWork/HomeWork02/Example/Example05_Seminar01/Program.cs
@@ -0,0 +1,8 @@
+﻿int randNum = new Random().Next(10, 100);
+int a = randNum % 10;
+int b = randNum / 10;
+
+int max = a;
+if(a < b) max = b;
+
+Console.WriteLine($"Максимальная цифра числа {randNum} -> {max}");
